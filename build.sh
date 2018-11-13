08770e131b2a4fc69444eb6412d08b5da47f885e
Mackenzie Salloum <mackenziesalloum@gmail.com>
2018-11-12 20:10:38 -0800

    Install node v10.

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
+NODE_VERSION="v10.12.0"
 cd $TRAVIS_BUILD_DIR/learning/shopping-list/
 
 source $NVM_DIR/nvm.sh
 
-nvm use v10.12.0
+nvm install $NODE_VERSION
+
+nvm use $NODE_VERSION
 
 npm install
 npm test
