8a20233ddb52ba4ed88e6ef1a16669be04691a76
Taylor Siemens <taylor@Taylors-MacBook-Pro.local>
2018-11-26 13:34:43 -0800

    The menu components for login and create a new account

--- src/LoginProperties/CreateAccountMenu.js
+++ src/LoginProperties/CreateAccountMenu.js
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import React from 'react';
 import '../LoginInterface.css';
 
-const CreateAccountMenu = () => {
 
+// const CreateAccountMenu = () => {
+
+class CreateAccountMenu extends Component {
+
+    render() {
     
     return(
-        <div className="lMenu">
-            <h1>Visual Course Planner</h1>
+        <div className="Menu">
+{/*             
+            // CURRENT COMMENTED OUT CODE MAKES THIS MENU DISAPPER.. ? 
+            {this.props.showmenu && 
+            <section id = "CreateAccountMenu"> */}
             <form>
+            <h1>Visual Course Planner</h1>
+            
             <input type="text" name="fname" placeholder="* First Name"/>
             <input type="text" name="lname" placeholder="* Last Name"/>   
             <input type="text" name="email" placeholder="* Email"/>
             <input type="text" name="pass" placeholder="* Password"/>   
             <input type="text" name="confirmPass" placeholder="* Confirm Password"/>
             <button className="createAccountbtn">Create Account</button> 
-            </form>
-                 
+            </form> 
+            {/* </section> */}
 
-                   
+            }
+
         </div>
 
     );
 }
+}
 
 
 export default CreateAccountMenu;
+
+
