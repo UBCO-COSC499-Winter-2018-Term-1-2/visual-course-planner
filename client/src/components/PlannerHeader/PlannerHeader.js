bed39a6ed67e8be0ab4c8d61b0985feca942ce42
Herraj Luhano <herrajluhano@gmail.com>
2018-11-29 10:31:17 -0800

    Adding favourite button component to VCP interface header component

--- client/src/components/PlannerHeader/PlannerHeader.js
+++ client/src/components/PlannerHeader/PlannerHeader.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import './PlannerHeader.css';
 import PlanName from '../PlanName/PlanName';
+import FavouriteBtn from '../FavouriteBtn/FavouriteBtn';
 
 const PlannerHeader = (props) => {
     return (
         <div className="planner-header-wrapper">
             <div className="plan-name-container"> <PlanName name="BA Major Computer Science"/> </div>
-            <div className="favouriteBtn-container"><p>Favourite Button</p></div>
+            <div className="favouriteBtn-container"><FavouriteBtn favourite={true}/></div>
             <div className="optimizeBtn-container"><p>Optimize button</p></div>
             <div className="warning-summary-container"><p>Warning summary</p></div>
             <div className="addcourseBtn-container"><p>Add course button</p></div>
