ce606efb6978d634997519c367bfded530c2f764
Herraj Luhano <herrajluhano@gmail.com>
2018-11-30 16:03:50 -0800

    Fix CSS for Course List Sidebar and Course info display components

--- client/src/components/CourseInfoDisplay/CourseInfoDisplay.js
+++ client/src/components/CourseInfoDisplay/CourseInfoDisplay.js
@@ -8,16 +8,13 @@ const CourseInfoDisplay = (props) => {
     return(
         <div className="courseinfo-wrapper">
             <div className="courseinfo-header-container">
-                <h4 className="course-title">
+                <h4 id="course-title">
                     COSC 111
                 </h4>
             </div>
             <div className="course-info-body-container">
-                <p className="course-info-body">
-                    Lorem Ipsum and other spiel
-                    asdasdoasasduiasgdiasugduiasgdasasdoi
-                    asoidhaosihdioashdoihasidhiashdoiashd
-                    asjdoihasoidhasoihdiashdoiahsdoihasd
+                <p id="course-info-body">
+                    This Course is the best course with the best prof Dr.Abdallah.
                 </p>
             </div>
         </div>
--- client/src/components/CourseListSideBar/CourseListSideBar.js
+++ client/src/components/CourseListSideBar/CourseListSideBar.js
@@ -5,22 +5,22 @@ import CloseSideBarBtn from '../CloseSideBarBtn/CloseSideBarBtn'
 import CourseInfoDisplay from '../CourseInfoDisplay/CourseInfoDisplay'
 
 const CourseListSideBar = (props) => {
-    let drawerClasses = 'side-drawer';
-    if (props.show) {
-      drawerClasses = 'side-drawer open';
-    }
     
+    let drawerClasses;
+    
+    //Show or hide side bar by changes 
+    props.show ? (drawerClasses = 'side-drawer open') : (drawerClasses = 'side-drawer')
     // adding search bar component to the side bar component
     // need to pass call back function as click prop
 
-
-    
     return (
       <div className={drawerClasses}>
         <CloseSideBarBtn click={props.close}/>
         <CourseSearchBar />
         {/* course search display results component goes here */}
-        <hr className="sidebar-divider"/>
+        <div className="sidebar-divider-container">
+          <hr id="sidebar-divider"/>
+        </div>
 
         {/* course info component goes here */}
         <CourseInfoDisplay />
