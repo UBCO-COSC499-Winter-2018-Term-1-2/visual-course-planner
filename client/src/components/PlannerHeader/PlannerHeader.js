341ea7f715d25548e73a0fec8a21a762969c40a2
Herraj Luhano <herrajluhano@gmail.com>
2018-11-30 19:18:28 -0800

    Add OptimizeBtn component to PlannerHeader, remove unused import

--- client/src/components/PlannerHeader/PlannerHeader.js
+++ client/src/components/PlannerHeader/PlannerHeader.js
@@ -1,19 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import './PlannerHeader.css';
 import PlanName from '../PlanName/PlanName';
 import FavouriteBtn from '../FavouriteBtn/FavouriteBtn';
-import SideBarToggleButton from '../SideBarToggleButton/SideBarToggleButton'; 
+import SideBarToggleButton from '../SideBarToggleButton/SideBarToggleButton';
+import OptimizeBtn from '../OptimizeBtn/OptimizeBtn'; 
 
 const PlannerHeader = (props) => {
     return (
         <div className="planner-header-wrapper">
             <div className="plan-name-container"> <PlanName name="BA Major Computer Science"/> </div>
             <div className="favouriteBtn-container"><FavouriteBtn favourite={true}/></div>
-            <div className="optimizeBtn-container"><p>Optimize button</p></div>
+            <div className="optimizeBtn-container"><OptimizeBtn click={props.optimize}/></div>
             <div className="warning-summary-container"><p>Warning summary</p></div>
             <div className="addcourseBtn-container"><SideBarToggleButton click={props.toggleSidebar}/></div>
         </div>
     );
+}
+
+PlannerHeader.propTypes = {
+    optimize: PropTypes.func.isRequired,
+    toggleSidebar: PropTypes.func.isRequired,
 }
 
 export default PlannerHeader;
