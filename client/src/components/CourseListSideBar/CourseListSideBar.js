53a6581aafbb4f34209d3a0cc98d730445d8f989
Herraj Luhano <herrajluhano@gmail.com>
2018-11-30 17:54:55 -0800

    Pass props to course Info display component in course list sidebar component

--- client/src/components/CourseListSideBar/CourseListSideBar.js
+++ client/src/components/CourseListSideBar/CourseListSideBar.js
@@ -7,23 +7,23 @@ import CourseInfoDisplay from '../CourseInfoDisplay/CourseInfoDisplay'
 const CourseListSideBar = (props) => {
     
     let drawerClasses;
-    
     //Show or hide side bar by changes 
     props.show ? (drawerClasses = 'side-drawer open') : (drawerClasses = 'side-drawer')
-    // adding search bar component to the side bar component
-    // need to pass call back function as click prop
-
+    
     return (
       <div className={drawerClasses}>
         <CloseSideBarBtn click={props.close}/>
+        
         <CourseSearchBar />
         {/* course search display results component goes here */}
         <div className="sidebar-divider-container">
           <hr id="sidebar-divider"/>
         </div>
 
-        {/* course info component goes here */}
-        <CourseInfoDisplay />
+        <CourseInfoDisplay 
+          title={props.courseTitle}
+          info={props.courseInfo}
+        />
       </div>
     );
 };
