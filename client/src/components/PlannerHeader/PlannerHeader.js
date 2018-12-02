6b77080379ce96dd349fc672e99dab854560ccd0
Herraj Luhano <herrajluhano@gmail.com>
2018-12-01 16:28:44 -0800

    Add Warning Summary component to Planner Header

--- client/src/components/PlannerHeader/PlannerHeader.js
+++ client/src/components/PlannerHeader/PlannerHeader.js
@@ -5,14 +5,18 @@ import PlanName from '../PlanName/PlanName';
 import FavouriteBtn from '../FavouriteBtn/FavouriteBtn';
 import SideBarToggleButton from '../SideBarToggleButton/SideBarToggleButton';
 import OptimizeBtn from '../OptimizeBtn/OptimizeBtn'; 
+import WarningSummary from '../WarningSummary/WarningSummary';
 
 const PlannerHeader = (props) => {
     return (
         <div className="planner-header-wrapper">
-            <div className="plan-name-container"> <PlanName name="BA Major Computer Science"/> </div>
+            <div className="plan-name-container"> <PlanName name={props.planName}/> </div>
             <div className="favouriteBtn-container"><FavouriteBtn favourite={true}/></div>
             <div className="optimizeBtn-container"><OptimizeBtn click={props.optimize}/></div>
-            <div className="warning-summary-container"><p>Warning summary</p></div>
+            <div className="warning-summary-container">
+                <WarningSummary 
+                    numberOfWarnings={props.numberOfWarnings}
+                    click={props.showWarning}/></div>
             <div className="addcourseBtn-container"><SideBarToggleButton click={props.toggleSidebar}/></div>
         </div>
     );
@@ -21,6 +25,7 @@ const PlannerHeader = (props) => {
 PlannerHeader.propTypes = {
     optimize: PropTypes.func.isRequired,
     toggleSidebar: PropTypes.func.isRequired,
+    numberOfWarnings: PropTypes.number.isRequired
 }
 
 export default PlannerHeader;
