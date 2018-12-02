f1b90d5cb1079fc77a9e6eadd2a8d21145c5e3cb
Mackenzie Salloum <mackenziesalloum@gmail.com>
2018-12-01 22:48:20 -0800

    Run deploy on success.

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+if [ $TRAVIS_BRANCH == 'master' ] ; then
+    # Initialize a new git repo in _site, and push it to our server.
+    cd client/build
+    git init
+        
+    git remote add deploy "deploy@visualcourseplanner.gq:/var/www/html"
+    git config user.name "Travis CI"
+    git config user.email "mackenziesalloum+travisCI@gmail.com"
+    
+    git add .
+    git commit -m "Deploy"
+    git push --force deploy master
+else
+    echo "Not deploying, since this branch isn't master."
+fi
