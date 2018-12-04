16878eb20f7dcc77ab6f314c38808d3debc20814
Mackenzie Salloum <mackenziesalloum@gmail.com>
2018-12-03 22:36:33 -0800

    Run script server sidee to pull changes.

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,18 @@
 set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     # Initialize a new git repo in client/build, and push it to the server.
-    cd client/build
-    git init
-        
-    git remote add deploy "deploy@visualcourseplanner.gq:/var/www/html"
-    git config user.name "Travis CI"
-    git config user.email "mackenziesalloum+travisCI@gmail.com"
-    
-    git add .
-    git commit -m "Deploy"
-    git push --force deploy master
+    ssh deploy@visualcourseplanner.gq <<EOF
+
+      cd /var/www/html
+      sudo git fetch --all
+      sudo git reset origin/master
+      cd client
+      npm install
+      cd ..
+      npm install
+      pm2 restart vcp
+EOF
+   
 else
     echo "Not deploying, since this branch isn't master."
 fi
