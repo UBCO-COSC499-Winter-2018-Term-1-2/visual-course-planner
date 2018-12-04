2bb117ef7c3f5ce4336b76728ff1616da1b12610
Mackenzie Salloum <mackenziesalloum@gmail.com>
2018-12-03 23:21:55 -0800

    Use travis to determine branch.

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
 set -x
-# if [ $TRAVIS_BRANCH == 'master' ] ; then
-    # Initialize a new git repo in client/build, and push it to the server.
-    ssh deploy@visualcourseplanner.gq <<EOF
 
-      cd /var/www/html
-      sudo git fetch --all
-      sudo git reset origin/master
-      cd client
-      npm install
-      cd ..
-      npm install
-      pm2 restart vcp
+ssh deploy@visualcourseplanner.gq <<EOF
+  cd /var/www/html
+  sudo git fetch --all
+  sudo git reset origin/master
+  cd client
+  npm install
+  cd ..
+  npm install
+  pm2 restart vcp
 EOF
-   
-# else
-    # echo "Not deploying, since this branch isn't master."
-# fi
+  
