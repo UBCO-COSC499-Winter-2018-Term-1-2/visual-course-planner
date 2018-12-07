e80f7d237c5fc74980eff358f0a72cb22b9258f0
Mackenzie Salloum <mackenziesalloum@gmail.com>
2018-12-07 13:58:03 -0800

    Need to reassign concatenated array.

--- server/src/services/WarningService.js
+++ server/src/services/WarningService.js
@@ -1,45 +1,45 @@
-const getStandingWarning = (user, course) => {
+function getStandingWarning(user, course) {
   let warnings = [];
   if (user.standing < course.standingRequirement) {
-    warnings.concat({
+    warnings.push({
       message: `${course.code} requires year ${course.standingRequirement}. Current standing: ${user.standing}`,
       type: "standing"
     });
   }
   return warnings;
-};
+}
 
-const getPrereqWarnings = (plan, course) => {
+function getPrereqWarnings(plan, course) {
   let warnings = [];
   const requirements = course.preRequisites;
   requirements.forEach(req => {
     const reqYearSemester = req.year.concat(req.semester);
     const courseYearSemester = course.year.concat(course.semester);
     if (!planHasCourse(plan, req) || reqYearSemester >= courseYearSemester) {
-      warnings.concat({
+      warnings.push({
         message: `${course.code} missing pre-requisite ${req.code}`,
         type: "prereq"
       });
     }
   });
   return ;
-};
+}
 
-const getCoreqWarnings = (plan, course) => {
+function getCoreqWarnings(plan, course) {
   let warnings = [];
   const requirements = course.coRequisites;
   requirements.forEach(req => {
     const reqYearSemester = req.year.concat(req.semester);
     const courseYearSemester = course.year.concat(course.semester);
     if (!planHasCourse(plan, req) || reqYearSemester >= courseYearSemester) {
-      warnings.concat({
+      warnings.push({
         message: `${course.code} missing co-requisite ${req.code}`,
         type: "coreq"
       });
     }
   });
   return warnings;
-};
+}
 
 function planHasCourse(plan, course) {
   return plan.courses.has(course.code);
@@ -48,27 +48,25 @@ function planHasCourse(plan, course) {
 module.exports = {
   getWarningsForCourse: (plan, user, course) => {
     let warnings = [];
-    warnings.concat(getStandingWarning(user, course));
-    warnings.concat(getCoreqWarnings(plan, course));
-    warnings.concat(getPrereqWarnings(plan, course));
-
+    warnings = warnings.concat(
+      getStandingWarning(user, course),
+      getCoreqWarnings(plan, course),
+      getPrereqWarnings(plan, course)
+    );
     return warnings;
   },
   
 
-  getWarnings: () => {
-    const courseWarnings = this.props.plan.courses.map(function(course) {
-      return this.getWarningsForCourse(this.props.plan, course);
-    }.bind(this));
-
-    Promise
-      .all(courseWarnings)
-      .then(warnings => {
-        return warnings;
-      })
-      .catch(err => {
-        console.log(err);
-      });
+  getWarnings: (plan, user) => {
+    let warnings = [];
+    plan.course.forEach(planCourse => {
+      warnings = warnings.concat(
+        getStandingWarning(user, planCourse),
+        getCoreqWarnings(plan, planCourse),
+        getPrereqWarnings(plan, planCourse)
+      );
+    });
+    return warnings;
   }
 
 };
