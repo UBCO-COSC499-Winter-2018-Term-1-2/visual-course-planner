de2bfd077819bdb6fde8d339dce145136bf8c18b
Herraj Luhano <herrajluhano@gmail.com>
2018-12-24 19:02:50 -0800

    Add hard coded state for semester Add drag and drop event handler functions Pass drag and drop event handlers as props to Semester component

--- client/src/components/Planner/PlannerArea.js
+++ client/src/components/Planner/PlannerArea.js
@@ -3,24 +3,114 @@ import PropTypes from 'prop-types';
 import PlannerHeader from '../PlannerHeader/PlannerHeader';
 import WarningSnackbar from '../WarningSnackbar/WarningSnackbar';
 import Semester from '../Semester/Semester';
-//import SemesterDivider from '../SemesterDivider/SemesterDivider';
 import './PlannerArea.css';
 
 class PlannerArea extends Component {
 
   state = {
     warnings: [],
-    showSnackbar: false
+    showSnackbar: false,
+    defaultTerms: [
+      {
+        number: "1",
+        coursesContained: ["COSC 111", "COSC 122"],
+        year: "2018",
+        session: "W"
+
+      },
+      {
+        number: "2",
+        coursesContained: ["COSC 121", "COSC 123"],
+        year: "2019",
+        session: "W"
+
+      },
+      {
+        number: "3",
+        coursesContained: ["COSC 222", "COSC 211"],
+        year: "2019",
+        session: "W"
+
+      },
+      {
+        number: "4",
+        coursesContained: ["COSC 221", "MATH 200"],
+        year: "2020",
+        session: "W"
+
+      },
+      {
+        number: "5",
+        coursesContained: ["COSC 111", "COSC 122"],
+        year: "2018",
+        session: "W"
+
+      },
+      {
+        number: "6",
+        coursesContained: ["COSC 111", "COSC 122"],
+        year: "2018",
+        session: "W"
+
+      },
+      {
+        number: "7",
+        coursesContained: ["COSC 111", "COSC 122"],
+        year: "2018",
+        session: "W"
+
+      },
+      {
+        number: "8",
+        coursesContained: ["COSC 111", "COSC 122"],
+        year: "2018",
+        session: "W"
+
+      }
+    ]
   }
 
-  //default semesters hard coded. Needs proper logic!
-  numberOfSemesters = [1, 2, 3, 4, 5, 6, 7, 8];
-
-  //rendering semester components by mapping above array
+  //rendering semester components by mapping defaulTerms state variable
   renderSemesters = () => {
-    return (this.numberOfSemesters.map((semester) =>
-      <Semester key={semester} term={semester} />
+    return (this.state.defaultTerms.map((term) =>
+      <Semester
+        key={term.number}
+        term={term.number}
+        coursesContained={term.coursesContained}
+        onCourseDragOver={this.onCourseDragOver}
+        onCourseDragStart={this.onCourseDragStart.bind(this)}
+        onCourseDrop={this.onCourseDrop} />
     ));
+  }
+
+  //drag over event handler for semester component - passed in as prop
+  onCourseDragOver = (e) => {
+    e.preventDefault();
+  }
+
+  //drag start event handler for course component - passed in as prop via Semester
+  //set data uses hard coded course code!
+  onCourseDragStart = (e, courseCode) => {
+    e.dataTransfer.setData("courseCode", courseCode);
+  }
+
+  //on drop event handler for semester component
+  onCourseDrop = (e, targetTerm) => {
+    let movedCourse = e.dataTransfer.getData("courseCode");
+    this.state.defaultTerms.filter((term) => {
+      if (term.number == targetTerm) {
+        //console.log(term.coursesContained);
+        this.setState({
+          defaultTerms: [
+            ...this.state.defaultTerms.slice(0,term.number-1),
+            Object.assign([], this.state.defaultTerms[term.number-1],term.coursesContained.push(movedCourse)),
+            ...this.state.defaultTerms.slice(term.number-1)
+          ]
+        });
+        //console.log(movedCourse + " to " + term.coursesContained);
+      }
+    });
+    console.log(this.state); 
   }
 
   showSnackbar = () => {
