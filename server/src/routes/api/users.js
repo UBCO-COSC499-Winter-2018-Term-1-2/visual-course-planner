3f75df4f6fff886cfb47c4decd61834c2b9d52f3
Noman Mohammad <noman0786@hotmail.com>
2019-02-24 17:59:55 -0800

    change format of data being posted

--- server/src/routes/api/users.js
+++ server/src/routes/api/users.js
@@ -7,13 +7,20 @@ const passport = require('passport');
 
 router.use(expressValidator()); // put the use in server.js and also import through npm?
 
+const redirectLogin = (req, res, next) => {
+  if (!req.session.userId){
+    res.redirect('/login')
+  }else{
+    next()
+  }
+}
 
 //user model
 const User = require('../../models/User');
 const user = new User();
 
 /**
- * @route POST api/users
+ * @route POST api/users/signup
  * @desc Insert a new user into the database
  * @access Private
  */ 
@@ -81,7 +88,14 @@ router.post('/signup', async (req, res) => {
 });
 
 
+/**
+ * @route POST api/users/login
+ * @desc authenticate a user
+ * @access Private
+ */ 
+
 router.post('/login', (req, res, next) => {
+  console.log('here now!');
   console.log(req.body);
   passport.authenticate('local', (err, user, info) => {
     console.log("info", info);
@@ -94,39 +108,44 @@ router.post('/login', (req, res, next) => {
   
 });
 
+/**
+ * @route POST api/users/coursehistory
+ * @desc Insert previous courses taken by user into database
+ * @access Private
+ */ 
 
-router.post('/signup/coursehistory', async (req, res) => {
-
+router.post('/:id/coursehistory', async (req, res) => {
   if(Object.keys(req.body).length === 0){
     console.log('no courses selected, nothing stored');
     res.status(200).send('no course history selected');
-}else{
-  // console.log(req.body);
-  // console.log(req.body.length);
-  var courses = {};
-  for(var key in req.body) {
-    if(req.body.hasOwnProperty(key)){
-      courses[key] = req.body[key];
+  }else{
+  let user_Id = req.params.id;
+  let courses = [];
+  for(let key in req.body) {
+    courses[key] = {
+      uid:user_Id,
+      cid: req.body[key]
     }
   }
-  //console.log(courses);
-  // var test = {
-  // uid: req.body.uid,
-  // cid: req.body.course
-  // };
-  for(var i in courses) {
+  console.log(courses);
+  for(let i in courses) {
     console.log(courses[i]);
     await user.insertCourses(courses[i]);
 }
-
   res.status(200).send('course(s) inserted for user');
 }
-
 });
 
+/**
+ * @route POST api/logout
+ * @desc end the users session
+ * @access Private
+ */
 
+router.post('/logout',redirectLogin, async (req, res) => {
 
-
-
+  console.log(req.session);
+ 
+  });
 
 module.exports = router;
