2511344044cdef3f496aedbfc1e6087dd7a766a3
Taylor Siemens <taylorjsie@gmail.com>
2019-03-05 17:34:38 -0800

    resolved create account connection in comfirmEmail.js

--- client/src/components/ConfirmEmail/ConfirmEmail.js
+++ client/src/components/ConfirmEmail/ConfirmEmail.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './ConfirmEmail.css';
-import '../Signup/CreateAccountMenu';
 import PropTypes from 'prop-types';
 
 import Input from '../Input/input';
